11c27e850395ea04be391be3ed03567a081b5f8c
Matthew McCants <mattmccants@gmail.com>
2012-08-07 21:20:54 -0500

    Made it so that the version is based on the commit.

--- ateoto_recipe/__init__.py
+++ ateoto_recipe/__init__.py
@@ -1,1 +1,10 @@
-__version__ = '0.0.2'
+import subprocess
+
+version = '0.0.2'
+
+try:
+    version = subprocess.check_output(['git','log','-1','--pretty="%h"']).strip('\"\n')
+except:
+    pass
+
+__version__ = version
